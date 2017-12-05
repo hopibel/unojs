29db063b820f9a3b007cd8dffa2ab535d52b6a10
Hope Ibelgaufts <hopibel@gmail.com>
2017-12-05 14:16:22 +0800

    Serve public folder

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const path = require('path');
@@ -7,6 +9,7 @@ require('./uno.js')(io);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
+app.use(express.static(path.join(__dirname, '/public')));
 
 http.listen(10000, () => {
   console.log('listening on *:10000');
